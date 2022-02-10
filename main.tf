751cfcfe31dc415f351f060fb4ccf74def60b623
Zachary Becker <zrbecker@gmail.com>
2022-02-10 13:55:15 -0800

    default acl and route tables for vpc

--- main.tf
+++ main.tf
@@ -14,6 +14,25 @@ provider "aws" {
 resource "aws_vpc" "eks_example_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    "Name" : "eks_example_vpc"
+    "Name" = "eks_example"
+  }
+}
+
+resource "aws_default_network_acl" "eks_example_default" {
+  default_network_acl_id = aws_vpc.eks_example_vpc.default_network_acl_id
+
+  # no rules defined, deny all traffic in this ACL
+  tags = {
+    "Name" = "eks_example"
+  }
+}
+
+resource "aws_default_route_table" "eks_example_default" {
+  default_route_table_id = aws_vpc.eks_example_vpc.default_route_table_id
+
+  route = []
+
+  tags = {
+    "Name" = "eks_example"
   }
 }
