97e3f3545c921ad7e49835491bac212bcae86fa8
Zachary Becker <zrbecker@gmail.com>
2022-02-11 06:16:12 -0800

    set up internet gateway for public network

--- main.tf
+++ main.tf
@@ -12,21 +12,21 @@ provider "aws" {
 }
 
 locals {
-  vpc_cidr_block = "10.0.0.0/16"
+  vpc_cidr_block = "10.48.0.0/16"
   public_subnets = {
     "eks_example_public_1" = {
-      cidr_block = "10.0.8.0/24"
+      cidr_block = "10.48.16.0/20"
     }
     "eks_example_public_2" = {
-      cidr_block = "10.0.9.0/24"
+      cidr_block = "10.48.32.0/20"
     }
   }
   private_subnets = {
     "eks_example_private_1" = {
-      cidr_block = "10.0.16.0/24"
+      cidr_block = "10.48.48.0/20"
     }
     "eks_example_private_2" = {
-      cidr_block = "10.0.17.0/24"
+      cidr_block = "10.48.64.0/20"
     }
   }
 }
@@ -41,14 +41,11 @@ resource "aws_vpc" "eks_example_vpc" {
 resource "aws_default_network_acl" "eks_example_default" {
   default_network_acl_id = aws_vpc.eks_example_vpc.default_network_acl_id
 
-  subnet_ids = concat(
-    [for s in aws_subnet.eks_example_private : s.id],
-    [for s in aws_subnet.eks_example_public : s.id]
-  )
+  subnet_ids = [for s in aws_subnet.eks_example_private : s.id]
 
   # no rules defined, deny all traffic in this ACL
   tags = {
-    "Name" = "eks_example"
+    "Name" = "eks_example_default"
   }
 }
 
@@ -58,8 +55,63 @@ resource "aws_default_route_table" "eks_example_default" {
   route = []
 
   tags = {
+    "Name" = "eks_example_default"
+  }
+}
+
+resource "aws_network_acl" "eks_example_public" {
+  vpc_id = aws_vpc.eks_example_vpc.id
+
+  subnet_ids = [for s in aws_subnet.eks_example_public : s.id]
+
+  ingress {
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    rule_no = 100
+  }
+
+  egress {
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    rule_no = 100
+  }
+
+  tags = {
+    "Name" = "eks_example_public"
+  }
+}
+
+resource "aws_internet_gateway" "eks_example" {
+  vpc_id = aws_vpc.eks_example_vpc.id
+
+  tags = {
     "Name" = "eks_example"
   }
+}
+
+resource "aws_route_table" "eks_example_public" {
+  vpc_id = aws_vpc.eks_example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_example.id
+  }
+
+  tags = {
+    "Name" = "eks_example_public"
+  }
+}
+
+resource "aws_route_table_association" "eks_example_public" {
+  for_each       = aws_subnet.eks_example_public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.eks_example_public.id
 }
 
 resource "aws_subnet" "eks_example_public" {
